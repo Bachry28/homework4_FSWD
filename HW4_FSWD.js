28eef088811c9169e01edffa4ef6d64ae5a7ef8e
Bachry28 <muhbahryalhadi@gmail.com>
2023-09-02 19:44:06 +0700

    Penambahan Perbandingan data

--- HW4_FSWD.js
+++ HW4_FSWD.js
@@ -26,7 +26,7 @@ function minimal(arr) {
 
   return minValue;
 }
-
+       
 function maksimal(arr) {
   let maxValue = arr[0];
 
@@ -41,20 +41,20 @@ function maksimal(arr) {
 function total(arr){
   let nilai = 0;
   for(i = 0; i<arr.length; i++){
-      nilai += arr[i]
+      nilai = nilai + arr[i]
   }
 
   return nilai
 }
 
 function rataRata(arr){
-  let nilai = 0;
-  for(i = 0; i<arr.length; i++){
-      nilai += arr[i]
-  }
-
   return total(arr) /arr.length
 }
+
+
+
+
+
 
 console.log("angka random:", nilairandom);
 console.log("angka genap:", angkaGenap);
@@ -67,4 +67,41 @@ console.log("angka ganjil:", angkaGanjil);
 console.log("angka maksimal di index ganjil:", maksimal(angkaGanjil));
 console.log("angka minimal di index ganjil:", minimal(angkaGanjil));
 console.log("total angka di index ganjil:", total(angkaGanjil));
-console.log("rata rata angka di index ganjil:", rataRata(angkaGanjil));
+console.log("rata rata angka di index ganjil:", rataRata(angkaGanjil))
+
+console.log ("Hasil Perbandingan nilai antara Array Genap dan Array Ganjil : ");
+
+if (minimal(angkaGenap) === minimal(angkaGanjil)) {
+    console.log("nilai minimal pada Array Genap dan Array Ganjil Sama")
+  } else if(minimal(angkaGenap) > minimal(angkaGanjil)) {
+    console.log("nilai minimal pada Array genap lebih besar dari Array ganjil")
+  }else {
+    console.log("nilai minimal pada Array ganjil lebih besar dari Array genap")
+  }
+
+  if (maksimal(angkaGenap) === maksimal(angkaGanjil)) {
+    console.log("nilai maksimal pada Array Genap dan Array Ganjil Sama")
+  } else if(maksimal(angkaGenap) > maksimal(angkaGanjil)) {
+    console.log("nilai maksimal pada Array genap lebih besar dari Array ganjil")
+  }else {
+    console.log("nilai maksimal Array ganjil lebih besar dari Array genap")
+  }
+
+  if (total(angkaGenap) === total(angkaGanjil)) {
+    console.log("nilai total pada Array Genap dan Array Ganjil Sama")
+  } else if(total(angkaGenap) > total(angkaGanjil)) {
+    console.log("nilai total pada Array genap lebih besar dari Array ganjil")
+  }else {
+    console.log("nilai total Array ganjil lebih besar dari Array genap")
+  }
+
+  
+  if (rataRata(angkaGenap) === rataRata(angkaGanjil)) {
+    console.log("nilai rata rata pada Array Genap dan Array Ganjil Sama")
+  } else if(total(angkaGenap) > total(angkaGanjil)) {
+    console.log("nilai rata rata pada Array genap lebih besar dari Array ganjil")
+  }else {
+    console.log("nilai rata rata Pada Array ganjil lebih besar dari Array genap")
+  }
+
+
